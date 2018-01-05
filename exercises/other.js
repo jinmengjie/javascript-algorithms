df9b8dc4a338cef2840e98e476755821ab0098b7
jinmengjie <jiduruxue@qq.com>
2018-01-05 17:45:44 +0800

    other

---
+++ exercises/other.js
@@ -0,0 +1,14 @@
+// 数组去重
+let uniqueArray = arr => {
+    let item, result = [], obj = {};
+    for (let i = 0; (item = arr[i]) !== undefined; i++) {
+        let key = JSON.stringify(item);
+        if (!obj[key]) {
+            result.push(item);
+            obj[key] = true;
+        }
+    }
+    return result;
+}
+// test
+uniqueArray([{a:'1'},2,3,4,{a:'1'},'2222','2222',{b:'2'},'adsad', {b:'2'}, {'a': {a:'1'}},{'a': {a:'1'}}]);
